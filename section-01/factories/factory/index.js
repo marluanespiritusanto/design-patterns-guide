3c208854873972b2b398f2b3b827e029e97abba7
Marluan <marluanespiritusantoguerrero@gmail.com>
2020-03-30 15:26:32 -0400

    [ADD] factory pattern added

---
+++ section-01/factories/factory/index.js
@@ -0,0 +1,8 @@
+const iPhoneXR = require("./iPhoneXR");
+const iPhoneXS = require("./iPhoneXS");
+
+const myiPhoneXR = new iPhoneXR("000123100");
+const myiPhoneXS = new iPhoneXS("2428346723");
+
+myiPhoneXR.displayConfig();
+myiPhoneXS.displayConfig();
